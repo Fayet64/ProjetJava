7f8e0f58b2b3e7feea8a5c92288d12656fcff92b
fayet64 <fayet64@fayetjulien>
2019-02-03 14:41:11 +0100

    Merge origin/master
    Conflicts:
    	src/projetjava/ProjetJava.java

--- src/projetjava/Chaine.java
+++ src/projetjava/Chaine.java
@@ -64,50 +64,46 @@ public void produire (int nvx, int n){
     GererFichier.getChaineProd().get(n);
     //regarder combien de produit on a besoin en entrée.BOF PAS BESOIN CAR ON CONNAIT DEJA LE CODE DE LA CHAINE 
      GererFichier.getChaineProd().get(n).entree.size();
-     GererFichier.getChaineProd().get(1).entree.get(elements.get(1));
+     GererFichier.getChaineProd().get(1).entree.get(GererFichier.getElements().get(1));
      
          
      // Gestion de la premiere chaine de production 
      
     //2eme etape pour chaque produit verifier que cette matiere premeire ou produit existe (le get(n) cest quel chaine qui va etre choisi 
-    if (GererFichier.getChaineProd().get(0).entree.get(elements.get(1)) > GererFichier.getElements().get(3).getQuantite()){
-        if (GererFichier.getElements().get(3).getAchat()==0)
+    if (GererFichier.getChaineProd().get(0).entree.get(GererFichier.getElements().get(3)) > GererFichier.getElements().get(3).getQuantite()){
+        if (GererFichier.getElements().get(3).getAchat()==0){
             System.out.println("Nous ne pouvons pas faire ce produit");
+        }
              else {
-                 double l = GererFichier.getChaineProd().get(0).entree.get(elements.get(1)) - GererFichier.getElements().get(3).getQuantite();
-                    Achat.ajoutQ(l);
+                 double l = GererFichier.getChaineProd().get(0).entree.get(GererFichier.getElements().get(1)) - GererFichier.getElements().get(3).getQuantite();
+                   // Achat.ajoutQ(l);
                 System.out.println("le produit a ete commandé");
                     }
-        
-        
-        
-    }else if (GererFichier.getChaineProd().get(0).entree.get(elements.get(2)) < GererFichier.getElements().get(1).getQuantite()){
+    }
+    if (GererFichier.getChaineProd().get(0).entree.get(GererFichier.getElements().get(2)) < GererFichier.getElements().get(1).getQuantite()){
         // la on rentre sur la deuxieme entree
-               if (GererFichier.getChaineProd().get(0).entree.get(elements.get(2)) > GererFichier.getElements().get(1).getQuantite()){
+               if (GererFichier.getChaineProd().get(0).entree.get(GererFichier.getElements().get(2)) > GererFichier.getElements().get(1).getQuantite()){
                     if (GererFichier.getElements().get(1).getAchat()==0)
                     System.out.println("Nous ne pouvons pas faire ce produit");
                }else{ 
-                 double l = GererFichier.getChaineProd().get(0).entree.get(elements.get(2)) - GererFichier.getElements().get(1).getQuantite();
-                    Achat.ajoutQ(l);
+                 double l = GererFichier.getChaineProd().get(0).entree.get(GererFichier.getElements().get(2)) - GererFichier.getElements().get(1).getQuantite();
+                   // Achat.ajoutQ(l);
                     System.out.println("le produit a ete commandé");
                     } 
-               
-               
-               
-               
-            }else if (GererFichier.getChaineProd().get(0).entree.get(elements.get(3)) < GererFichier.getElements().get(2).getQuantite()){
+    }
+    if (GererFichier.getChaineProd().get(0).entree.get(GererFichier.getElements().get(3)) < GererFichier.getElements().get(2).getQuantite()){
                     // la on rentre sur la troisieme  entree
-                         if (GererFichier.getChaineProd().get(0).entree.get(elements.get(3)) > GererFichier.getElements().get(2).getQuantite()){
+                         if (GererFichier.getChaineProd().get(0).entree.get(GererFichier.getElements().get(3)) > GererFichier.getElements().get(2).getQuantite()){
                              if (GererFichier.getElements().get(1).getAchat()==0)
                                 System.out.println("Nous ne pouvons pas faire ce produit");
                          }else{ 
-                             double l = GererFichier.getChaineProd().get(0).entree.get(elements.get(3)) - GererFichier.getElements().get(2).getQuantite();
-                              Achat.ajoutQ(l);
+                             double l = GererFichier.getChaineProd().get(0).entree.get(GererFichier.getElements().get(3)) - GererFichier.getElements().get(2).getQuantite();
+                              //Achat.ajoutQ(l);
                             System.out.println("le produit a ete commandé");
                              } 
                          
             } else {
-                double newQuante= GererFichier.getChaineProd().get(0).sortie.get(element(1));
+                double newQuante= GererFichier.getChaineProd().get(0).sortie.get(GererFichier.getElements().get(1));
                 
                 // il faut une methode qui prend la quantie de lelement produit et lajouter au elements deja present dans element du coup ça fera un truc du genre pour la chiane de prod 1 
                 // quantite du produit e005 + 1
@@ -115,9 +111,9 @@ public void produire (int nvx, int n){
                      GererFichier.getElements().get(6).setQuantite(GererFichier.getElements().get(6).getQuantite()+newQuante*nvx) ;
                      
                 // retirer les quantité 
-                        double newQuante1= GererFichier.getChaineProd().get(0).entree.get(element(1));
-                        double newQuante2= GererFichier.getChaineProd().get(0).entree.get(element(2));
-                        double newQuante3= GererFichier.getChaineProd().get(0).entree.get(element(3));
+                        double newQuante1= GererFichier.getChaineProd().get(0).entree.get(GererFichier.getElements().get(1));
+                        double newQuante2= GererFichier.getChaineProd().get(0).entree.get(GererFichier.getElements().get(2));
+                        double newQuante3= GererFichier.getChaineProd().get(0).entree.get(GererFichier.getElements().get(3));
                         
                      GererFichier.getElements().get(6).setQuantite(GererFichier.getElements().get(3).getQuantite()-newQuante1*nvx);
                      GererFichier.getElements().get(6).setQuantite(GererFichier.getElements().get(1).getQuantite()-newQuante2*nvx);
--- src/projetjava/ProjetJava.java
+++ src/projetjava/ProjetJava.java
@@ -18,6 +18,7 @@ public class ProjetJava {
         // TODO code application logic here
         //fayet
         //test2kokia
+        
     }
         
 }
